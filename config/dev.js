7d7718085ecd55483d253f05e3f77bb4122f4216
Russell Rosario <rosariorussell@gmail.com>
2018-07-23 15:18:45 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-        '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://russell:pprsa123@ds147391.mlab.com:47391/rr-advanced-node',
-  cookieKey: '123123123'
-}
